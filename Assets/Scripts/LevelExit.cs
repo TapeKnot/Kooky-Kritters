5b4ae60ff36ab9a4efc922e5355abff9ecad7936
brupesh999 <bhadra.rupesh@gmail.com>
2024-10-29 22:22:56 -0400

    added level exit and space gem

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+
+public class LevelExit : MonoBehaviour
+{
+    public GameObject player;
+    private bool isExitAccessible = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isExitAccessible = false;
+    }
+    public void UnlockExit()
+    {
+        isExitAccessible = true;
+        Debug.Log("The exit is now unlocked!");
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision");
+        // Only log when the player collides with the "Vine" (X block)
+        if (!isExitAccessible &&
+            collision.gameObject.name == "Vine" &&
+            collision.gameObject == player)
+        {
+            Debug.Log("The exit is blocked! Destroy the space gem to unblock.");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Debug.Log("Collided with: " + collision.name);
+        // Check if the object entering the trigger is the player
+        if (collision.transform.root.gameObject == player) {
+            if (isExitAccessible)
+            {
+                LevelComplete();
+            }
+        }
+    }
+
+    private void LevelComplete()
+    {
+        Debug.Log("Congratulations! You've completed the level.");
+    }
+
+}
