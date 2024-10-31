b825410da78fed2e9c0f14fc6282eee1a61de5d4
TapeKnot <benelevy@icloud.com>
2024-10-31 18:57:27 -0400

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -54,7 +54,13 @@ public class Player : MonoBehaviour
 
             Vector3 offsetPos = movePoint + offset;
 
-            if (offset.magnitude == 1f && IsInBounds(offsetPos))
+            if (offset.magnitude == 1f && IsMoveableTile(offsetPos))
+            {
+                movePoint = offsetPos;
+            }
+
+
+
             {
                 if (critterMode)
                 {
@@ -122,13 +128,42 @@ public class Player : MonoBehaviour
     // Checks if position is on a moveable tile
     private bool IsMoveableTile(Vector3 position)
     {
-        return (!Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Water")) || Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("WaterBug"))) && (!Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Hole"))
-            || Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Centipede")));
+        // For movement:
+        // Player can move on ground, mud, holes (if centipede is on top), and water (if water bug is on top)
+        // Player CANNOT move on out-of-bounds, holes, and water
+        // Centipedes CANNOT be placed on water or out-of-bounds
+        // Water Bugs CANNOT be placed on holes or out-of-bounds
+        // Both can be placed on mud to negate its effects
+        bool isGround = Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Ground"));
+        bool isMud = Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Mud"));
+        bool isCoveredHole = Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Hole")) && Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Centipede"));
+        bool isCoveredWater = Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Water")) && Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("WaterBug"));
+        return isGround || isMud || isCoveredHole || isCoveredWater;
+        // But wait, this means that as long as a ground tile is there, anything is moveable...make a note that each grid space should have at most 1 tile layer normally.
+
+        //return (!Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Water")) || Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("WaterBug"))) && (!Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Hole"))
+          //  || Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Centipede")));
     }
 
     // Checks if position is in world bounds AKA on ground tile
     private bool IsInBounds(Vector3 position)
     {
         return Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Ground"));
+    }
+
+    // Checks if a ONE-TILE position is a valid critter placement
+    // NOTE: Does not check for multi-tile placements (ahem, Centipede, ahem). How do we address that?
+    private bool IsPlaceableTile(Vector3 position)
+    {
+        // All critters can be placed on ground, mud, covered holes, and covered water.
+        // Critters CANNOT be placed out-of-bounds
+        // Critters placed on holes or water will normally fall or drown, respectively.
+        // The Water Bug can be placed on water.
+        // The Centipede can be placed on holes.
+        // The Fly can be placed on water or holes, it flies over everything.
+        bool isGround = Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Ground"));
+        bool isMud = Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Mud"));
+        bool isHole = Physics2D.OverlapCircle(position, 0.2f, LayerMask.GetMask("Hole"));
+        bool isWater = 
     }
 }
