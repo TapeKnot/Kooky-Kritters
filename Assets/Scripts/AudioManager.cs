7111b2678441b44581fe4f973518aaee5e9be0b3
dannycastroaudio <dannycastroaudio@gmail.com>
2024-11-01 17:01:52 -0400

    water bug

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,6 +8,7 @@ public class AudioManager : MonoBehaviour
 [Header("---------- Audio Clip ----------")] 
 public AudioClip footstepsDirt;
 public AudioClip gemBreak;
+public AudioClip waterBug;
 
 
  public void PlaySFX(AudioClip clip)
